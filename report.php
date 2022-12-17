907a45631fb7acf11853eb8d6a37b47ea1e89d04
crimson tiangco <cftiangco@gmail.com>
2022-12-17 09:31:42 +0800

    enhanced pdf report

--- report.php
+++ report.php
@@ -60,7 +60,7 @@ class PDF extends FPDF
 
     function gridRow($data)
     {
-        $this->SetFont('Arial', '', 9);
+        $this->SetFont('Arial', '', 6);
         $this->setX(11);
         $this->Cell(40, 7, $data->lastname . ', ' . $data->firstname . ' ' . $data->middlename, 'B', 0, 0, '');
 
@@ -71,28 +71,28 @@ class PDF extends FPDF
         $this->Cell(9, 7, getAge($data->birthday), 'B', 0, 0, '');
 
         $this->setX(102);
-        $this->Cell(35, 7, $data->studying . ' / ' . $data->grade, 'B', 0, 0, '');
+        $this->Cell(35, 7, strtoupper($data->studying) . ' / ' . $data->grade, 'B', 0, 0, '');
 
         $this->setX(145);
         $this->Cell(15, 7, $data->occupation . ' / ' . $data->salary, 'B', 0, 0, '');
 
         $this->setX(183);
-        $this->Cell(10, 7, $data->breast_feeding, 'B', 0, 0, '');
+        $this->Cell(10, 7, strtoupper($data->breast_feeding), 'B', 0, 0, '');
 
         $this->setX(213);
-        $this->Cell(10, 7, $data->bottle_feeding, 'B', 0, 0, '');
+        $this->Cell(10, 7, strtoupper($data->bottle_feeding), 'B', 0, 0, '');
 
         $this->setX(240);
-        $this->Cell(10, 7, $data->mix_feeding, 'B', 0, 0, '');
+        $this->Cell(10, 7, strtoupper($data->mix_feeding), 'B', 0, 0, '');
 
         $this->setX(267);
-        $this->Cell(10, 7, $data->philhealth_member, 'B', 0, 0, '');
+        $this->Cell(10, 7, strtoupper($data->philhealth_member), 'B', 0, 0, '');
 
         $this->setX(291);
-        $this->Cell(23, 7, $data->disability . ' / ' . $data->disability_type, 'B', 0, 0, '');
+        $this->Cell(23, 7, strtoupper($data->disability) . ' / ' . $data->disability_type, 'B', 0, 0, '');
 
         $this->setX(320);
-        $this->Cell(10, 7, $data->sex, 'B', 1, 0, '');
+        $this->Cell(10, 7, strtoupper($data->sex), 'B', 1, 0, '');
     }
 }
 
@@ -103,169 +103,169 @@ $pdf->SetFont('Arial', '', 9);
 
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(30, 7, 'Barangay / Purok:', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->purok, 'B', 0, '');
 
 $pdf->setX('285');
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(30, 7, 'Petsa ng Survey', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(25, 5, dateFormat($data->created_at), 'B', 1, '');
 
-$pdf->Ln(5);
+$pdf->Ln(1);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(15, 7, 'HH No.:', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->hh_no, 'B', 0, '');
 
 $pdf->setX(60);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(25, 7, 'Family A/B/C/D', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->family_type, 'B', 0, '');
 
 $pdf->setX(120);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(40, 7, 'Miyembron ng HH [dami]', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->family_members, 'B', 0, '');
 
 $pdf->setX(200);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(35, 7, 'Kompletong Address', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->complete_address, 'B', 0, '');
 
-$pdf->Ln(5);
+$pdf->Ln(10); // 2nd row
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(32, 7, 'HOUSEHOLD HEAD', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->household_head, 'B', 0, '');
 
 $pdf->setX(75);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(10, 7, 'Bday', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(15, 5, dateFormat($data->household_head_birthday), 'B', 0, '');
 
 $pdf->setX(103);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(10, 7, 'Edad', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(15, 5, $data->household_head_age, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(5, 5, getAge($data->household_head_birthday), 'B', 0, '');
 
-$pdf->setX(130);
+$pdf->setX(120);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(51, 7, 'Nag-aral [Y/N] GRADE/HS/College', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(15, 5, $data->household_head_student . ' / ' . $data->household_head_student_grade, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(15, 5, strtoupper($data->household_head_student) . ' / ' . $data->household_head_student_grade, 'B', 0, '');
 
 $pdf->setX(198);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(15, 7, 'Trabaho', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->household_head_occupation, 'B', 0, '');
 
 $pdf->setX(247);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(27, 7, 'Buwanang Sahod', 0, 0, '');
-$pdf->SetFont('Arial', '', 6);
-$pdf->Cell(15, 5, $data->household_head_salary, 'B', 0, '');
+$pdf->Cell(30, 7, 'Buwanang Sahod', 0, 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->MultiCell(12, 2.5, $data->household_head_salary, 'B', 0, '');
 
 $pdf->setX(290);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(38, 7, 'PhilHealth Member[Y/N]', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(13, 5, $data->household_head_philhealth_member, 'B', 1, '');
+$pdf->Cell(38, -2, 'PhilHealth Member[Y/N]', 0, 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(14, 1, strtoupper($data->household_head_philhealth_member), 'B', 1, '');
 
 $pdf->setX(20);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(57, 7, 'May Disability [Y/N] anong disability', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(25, 5, $data->household_head_disability . ' / ' . $data->household_head_disability_type, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(25, 5, strtoupper($data->household_head_disability) . ' / ' . $data->household_head_disability_type, 'B', 0, '');
 
 $pdf->setX(105);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(10, 7, 'M/F:', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(15, 6, $data->household_head_gender, 'B', 1, '');
 
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(15, 7, 'ASAWA', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(40, 5, $data->partner_name, 'B', 0, '');
 
 $pdf->setX(67);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(10, 7, 'Bday', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(15, 5, dateFormat($data->partner_birthday), 'B', 0, '');
 
 $pdf->setX(95);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(10, 7, 'Edad', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(10, 5, $data->partner_age, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(5, 5, getAge($data->partner_birthday), 'B', 0, '');
 
-$pdf->setX(117);
+$pdf->setX(112);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(52, 7, 'Nag-aral [Y/N] GRADE/HS/College', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(18, 5, $data->partner_student . ' / ' . $data->partner_grade, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(18, 5, strtoupper($data->partner_student) . ' / ' . $data->partner_grade, 'B', 0, '');
 
 $pdf->setX(190);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(15, 7, 'Trabaho', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(30, 5, $data->partner_occupation, 'B', 0, '');
 
 $pdf->setX(247);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(27, 7, 'Buwanang Sahod', 0, 0, '');
+$pdf->Cell(30, 7, 'Buwanang Sahod', 0, 0, '');
 $pdf->SetFont('Arial', '', 6);
-$pdf->Cell(15, 5, $data->partner_salary, 'B', 0, '');
+$pdf->MultiCell(10, 2, $data->partner_salary, 'B', 0, '');
 
 $pdf->setX(290);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(38, 7, 'PhilHealth Member[Y/N]', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(13, 5, $data->partner_philhealth_member, 'B', 1, '');
+$pdf->Cell(38, 1, 'PhilHealth Member[Y/N]', 0, 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(13, 2.5, strtoupper($data->partner_philhealth_member), 'B', 1, '');
 
 $pdf->setX(20);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(45, 7, 'Buntis [Y/N] Age of Gestation', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(10, 5, $data->partner_pregnant . ' / ' . $data->partner_age_of_gestation, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(10, 5, strtoupper($data->partner_pregnant) . ' / ' . $data->partner_age_of_gestation, 'B', 0, '');
 
 $pdf->setX(75);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(6.9, 7, 'LMP', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(10, 5, dateFormat($data->lmp), 'B', 0, '');
+$pdf->Cell(8, 7, 'LMP', 0, 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(15, 5, dateFormat($data->lmp), 'B', 0, '');
 
 $pdf->setX(103);
 $pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(7, 7, 'EDC', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
+$pdf->Cell(8, 7, 'EDC', 0, 0, '');
+$pdf->SetFont('Arial', '', 7);
 $pdf->Cell(10, 5, dateFormat($data->edc), 'B', 0, '');
 
 $pdf->setX(130);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(23, 7, 'Nagpapasuso:', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(10, 5, $data->breast_feeding, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(10, 5, strtoupper($data->breast_feeding), 'B', 0, '');
 
 $pdf->setX(163);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(93, 7, 'Gumagamit ng Family Planning Method [Y/N] anong method', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(16, 5, $data->family_planning_method . ' / ' . $data->family_planning_methodtype, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(16, 5, strtoupper($data->family_planning_method) . ' / ' . $data->family_planning_methodtype, 'B', 0, '');
 
 $pdf->setX(273);
 $pdf->SetFont('Arial', 'B', 9);
 $pdf->Cell(56, 7, 'May disability [Y/N] anong disability', 0, 0, '');
-$pdf->SetFont('Arial', '', 9);
-$pdf->Cell(12, 5, $data->disability . ' / ' . $data->disability_type, 'B', 0, '');
+$pdf->SetFont('Arial', '', 7);
+$pdf->Cell(12, 5, strtoupper($data->disability) . ' / ' . $data->disability_type, 'B', 0, '');
 
 $pdf->Ln(5);
 $pdf->SetFont('Arial', 'B', 9);
